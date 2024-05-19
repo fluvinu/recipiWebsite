6fc10bea0ad3fdede8cc4e731e80a2dd11690dbb
Siddharth Shivwanshi <65756060+fluvinu@users.noreply.github.com>
2024-05-19 13:35:01 +0530

    Update Navbar.jsx

--- src/Racipi Project/Navbar.jsx
+++ src/Racipi Project/Navbar.jsx
@@ -4,15 +4,17 @@ import "./Navbar.css"
 const Navbar = () => {
   return (
     <div className='Navbar'>
-        <nav>
+        
             <div className="logo">
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR90odnAyfjLDbG7u6m-1v7pBTJc7CLKOQTv2MjOaxF7Q&s" width={"100px"} height={"100px"} alt="" />
-            </div>
+              </div>
+            
             <div className="Link">
                 <Link to={'/'}> Home </Link>
                 <input type="text" placeholder='search rechipes' />
             </div>
-        </nav>
+           
+        
       
     </div>
   )
