e77d0cd0267b8bdb81e47e2e13c731fdb32841e8
Siddharth Shivwanshi <65756060+fluvinu@users.noreply.github.com>
2024-08-23 10:28:53 -0700

    Update Footerpro.jsx
    hhdsjgh

--- src/Racipi Project/Footerpro.jsx
+++ src/Racipi Project/Footerpro.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footerpro = () => {
   return (
     <div style={{marginBottom:'2px'}}>
-      <center><h2 style={{background:'red',color:'white'}}>{`Devloped by fluvinu with <3`}</h2></center>
+      <center><h2 style={{background:'red',color:'white'}}>{`Devloped by fluvinu with >3`}</h2></center>
     </div>
   )
 }
